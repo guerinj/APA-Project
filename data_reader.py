dbd26067adc4fee4d5beff167c96eba066a4568f
Pol Maire <polmaire@gmail.com>
2014-03-17 17:21:58 +0100

    Modified DataReader in order to remove stopwords from universe. Added data/stopwords w/ a file with all the stopwords

--- data_reader.py
+++ data_reader.py
@@ -7,7 +7,7 @@ class DataReader(object):
 
     universe = list()
 
-    def __init__(self, datasource):
+    def __init__(self, datasource, stopwords_source=None):
         words = set()
         with open(datasource, 'r') as data:
             for line in data:
@@ -16,6 +16,12 @@ class DataReader(object):
                 for word in new_words:
                     if word.strip() not in string.punctuation:
                         words = words.union(set([word.strip().lower()]))
+        if stopwords_source:
+            stopwords = set()
+            with open(stopwords_source, 'r') as data:
+                for line in data:
+                    stopwords = stopwords.union(set([line.strip().lower()]))
+            words = words.difference(stopwords)
         self.universe = list(words)
 
     def get_sentence_coordinates(self, sentence):
@@ -36,6 +42,6 @@ class DataReader(object):
 
 
 if __name__ == '__main__':
-    datareader = DataReader()
+    datareader = DataReader('data/test_data/test.data', 'data/stopwords/stopwords.txt')
     sentence = "7549 | 0.84722 | insightfully written , delicately performed"
     print datareader.get_sentence_coordinates(sentence)
